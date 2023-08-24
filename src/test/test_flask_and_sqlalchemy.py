550079951f937c54a5306fce66d79111f7ba62fa
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-08-24 23:47:42 +0800

    Update codes

---
+++ src/test/test_flask_and_sqlalchemy.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
+
+if __name__ == '__main__':
+    app.run(debug=True)
